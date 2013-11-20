aad9cc4b2f46df091bb5e0c751d257b5848f061d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2013-11-20 12:19:56 +0200

    Fix

--- components/modules/Home/trigger.php
+++ components/modules/Home/trigger.php
@@ -22,7 +22,7 @@ Trigger::instance()->register(
 	'HybridAuth/add_session/after',
 	function () use ($driver) {
 		$User	= User::instance();
-		if ($User->get_data('driver') === false) {
+		if ($User->get_data('driver') === false) {file_put_contents(DIR.'/test', var_export($driver, true));
 			$User->set_data('driver', $driver);
 			Drivers::instance()->add($User->id);
 			Mail::instance()->send_to(
