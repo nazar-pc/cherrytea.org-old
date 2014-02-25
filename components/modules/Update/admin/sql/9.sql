9a9dacfa326e5469d245804c98fb90a0cae5aee0
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-02-25 18:36:49 +0100

    Tiny fix

--- components/modules/Update/admin/sql/9.sql
+++ components/modules/Update/admin/sql/9.sql
@@ -1,5 +1,2 @@
-ALTER TABLE `[prefix]goods` ADD `date` VARCHAR( 255 ) NOT NULL ,
-ADD `time` VARCHAR( 255 ) NOT NULL ,
-ADD `coordinates` VARCHAR( 255 ) NOT NULL ;
 ALTER TABLE `[prefix]goods` ADD `address` VARCHAR( 255 ) NOT NULL AFTER `time` ;
 ALTER TABLE `[prefix]goods` ADD `phone` VARCHAR( 255 ) NOT NULL ;
