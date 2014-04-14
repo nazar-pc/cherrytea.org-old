27c0239abeaa237b19b004f37bcd256618ef4fd2
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2014-04-14 16:42:50 +0300

    Removed sharing block

--- themes/CherryTea/js/script.coffee
+++ themes/CherryTea/js/script.coffee
@@ -6,17 +6,3 @@
  * @license		MIT License, see license.txt
 ###
 
-social_block	= $('.social-block')
-fb				= social_block.find('.fb')
-fb
-	.click ->
-		window.open(
-			"//www.facebook.com/sharer.php?u=https\u00253A\u00252F\u00252Fwww.facebook.com\u00252Fcherrytea.org&display=popup",
-			'cherrytea-share-fb',
-			'height=340, left=' + ($(window).width() / 2 - 335) + ', location=no, toolbar=no, top=' + ($(window).height() / 2 - 170) + ',width=670'
-		)
-$.getJSON(
-	'http://graph.facebook.com/' + 'https://www.facebook.com/cherrytea.org',
-	(json) ->
-		fb.html(json.likes)
-)
--- themes/CherryTea/js/script.js
+++ themes/CherryTea/js/script.js
@@ -10,18 +10,7 @@
 
 
 (function() {
-  var fb, social_block;
 
-  social_block = $('.social-block');
 
-  fb = social_block.find('.fb');
-
-  fb.click(function() {
-    return window.open("//www.facebook.com/sharer.php?u=https\u00253A\u00252F\u00252Fwww.facebook.com\u00252Fcherrytea.org&display=popup", 'cherrytea-share-fb', 'height=340, left=' + ($(window).width() / 2 - 335) + ', location=no, toolbar=no, top=' + ($(window).height() / 2 - 170) + ',width=670');
-  });
-
-  $.getJSON('http://graph.facebook.com/' + 'https://www.facebook.com/cherrytea.org', function(json) {
-    return fb.html(json.likes);
-  });
 
 }).call(this);
